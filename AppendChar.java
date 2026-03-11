9dcfa375dc6ad9b51e486f0425ba4ee4fdc454d1
Priyadharsini P <priyadharsini162@gmail.com>
2026-03-11 21:27:45 +0530

    Add files via upload

---
+++ AppendChar.java
@@ -0,0 +1,23 @@
+class AppendChar {
+
+    public static int appendCharacters(String s, String t) {
+        int i=0;
+        int j=0;
+
+        while (i<s.length()&&j<t.length()) {
+            if (s.charAt(i)==t.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return t.length() - j;
+    }
+    public static void main(String[] args) {
+
+        String s = "coaching";
+        String t = "coding";
+
+        int result = appendCharacters(s, t);
+        System.out.println(result);
+    }
+}
