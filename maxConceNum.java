e3c2277f5228161d55c454ca5382158e944aa829
Priyadharsini P <priyadharsini162@gmail.com>
2026-03-11 22:12:53 +0530

    Add files via upload

---
+++ maxConceNum.java
@@ -0,0 +1,27 @@
+    class maxConceNum{
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                count++;
+                if(count>max)
+                {
+                    max=count;
+                }
+            }
+            else
+            {
+                count=0;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args)
+    {
+        int[] nums={1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+    }
