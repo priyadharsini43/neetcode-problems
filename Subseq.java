9d5e1f00f29beaf438c00df5fca6cf1fcbaf90bc
Priyadharsini P <priyadharsini162@gmail.com>
2026-03-11 06:42:35 +0530

    Add files via upload

---
+++ Subseq.java
@@ -0,0 +1,21 @@
+class Subseq{
+    public static boolean isSubsequence(String s, String t) {
+        int i=0,j=0;
+        while(i<s.length()&&j<t.length())
+        {
+            if(s.charAt(i)==t.charAt(j))
+            {
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+public static void main(String[] args)
+{
+    String s="abc";
+    String t="gakbec";
+    System.out.print(isSubsequence(s,t));
+
+}
+}
